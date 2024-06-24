6052621a3e6937cba4e0f0dd82bf6ad6a6ad9dfe
Praise <praisexi@outlook.com>
2024-06-24 13:53:32 +0100

    chore: Refactor Jenkinsfile.groovy for improved readability and consistency

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,17 +7,18 @@ pipeline {
                 echo 'Testing... - This should complete'
             }
         }
-    
+
         stage('Echo Env Variables') {
             steps {
-		script {
-			def prTitle = env.CHANGE_TITLE
-			def prDesc = env.CHANGE_DESCRIPTION                
-			echo 'PR TITLE:'
-			echo prTitle
-			echo 'prDescription'
-			echo prDesc
-		}
+                script {
+                    def prTitle = env.CHANGE_TITLE
+                    def prDesc = env.CHANGE_DESCRIPTION
+                    echo 'PR TITLE:'
+                    echo prTitle
+                    echo 'prDescription'
+                    echo prDesc
+                    echo "PR: ${prTtitle}"
+                }
             }
         }
     }
