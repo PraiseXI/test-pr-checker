939ec0cea1acbe03f21eedc3eccf897722fd84ea
Praise <praisexi@outlook.com>
2024-06-28 10:14:17 +0100

    chore: Trim PR title and description in Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
                     def prTitle = env.CHANGE_TITLE
                     def prDesc = env.CHANGE_DESCRIPTION
                     echo 'PR TITLE: ' + prTitle
-                    echo 'prDescription ' + prDesc
+                    echo 'PR DESCRIPTION: ' + prDesc
                 }
             }
         }
@@ -17,7 +17,8 @@ pipeline {
         stage('Check PR Title') {
             steps {
                 script {
-                    def prTitle = env.CHANGE_TITLE
+                    def prTitle = env.CHANGE_TITLE?.trim() // Trim to remove any leading/trailing spaces
+                    echo 'Trimmed PR TITLE: ' + prTitle
 
                     // The title pattern ensures that the title starts with "feat: SMARTJRNYS-" or "fix: SMARTJRNYS-"
                     def titlePattern = ~/(?i)^(feat: SMARTJRNYS-|fix: SMARTJRNYS-).*/
@@ -25,16 +26,18 @@ pipeline {
                     // Validate title
                     if (titlePattern.matcher(prTitle).find()) {
                         echo 'Title all good'
-            } else {
+                    } else {
                         error "PR title does not match the required format (must start with 'feat: SMARTJRNYS-' or 'fix: SMARTJRNYS-')"
                     }
                 }
             }
         }
+
         stage('Check PR Description') {
             steps {
                 script {
-                    def prDescription = env.CHANGE_DESCRIPTION
+                    def prDescription = env.CHANGE_DESCRIPTION?.trim() // Trim to remove any leading/trailing spaces
+                    echo 'Trimmed PR DESCRIPTION: ' + prDescription
 
                     // The description pattern ensures it contains a link with the base URL
                     def descriptionPattern = ~/(?i)https:\/\/jira\.devops\.lloydsbanking\.com\/browse\/SMARTJRNYS\S*/
@@ -42,7 +45,7 @@ pipeline {
                     // Validate description
                     if (descriptionPattern.matcher(prDescription).find()) {
                         echo 'Description all good'
-            } else {
+                    } else {
                         error 'PR description does not contain the required Jira link'
                     }
                 }
